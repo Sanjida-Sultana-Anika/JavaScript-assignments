4063903e6822753943b7f40250b2030de614279d
LENOVO <sanjida.sultana.anika@gmail.com>
2023-11-26 23:07:34 +0200

    m2_answer update

--- Module2/module2_assignment1/m2_answer1.js
+++ Module2/module2_assignment1/m2_answer1.js
@@ -6,6 +6,7 @@ for (let i=0; i<5; i++) {
 }
 
 document.write("<p>Numbers in reverse order: </p>");
+number.sort((a, b) => b - a);
 for (let i = number.length - 1; i >= 0; i--) {
   document.write("<p>" + number[i] + "</p>");
 }
